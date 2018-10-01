7b198a4b13216e70148be4b29a18c43229d634cc
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-10-01 08:18:13 -0400

    Make sure umask is properly applied on all files and folders.

--- makemkv-builder/builder/umask_wrapper.c
+++ makemkv-builder/builder/umask_wrapper.c
@@ -7,14 +7,17 @@
 static void init(void) __attribute__((constructor));
 
 static int (*orig_open)(const char *path, int flags, mode_t mode);
+static int (*orig_mkdir)(const char *pathname, mode_t mode);
 
 /*
  * Constructor.
  *
  * Save pointer to the original open function.
  */
-static void init(void) {
+static void init(void)
+{
     orig_open = dlsym(RTLD_NEXT, "open");
+    orig_mkdir = dlsym(RTLD_NEXT, "mkdir");
 }
 
 /*
@@ -22,12 +25,15 @@ static void init(void) {
  *
  * If we are opening an MKV file, override the mode.
  */
-int open(const char *path, int flags, mode_t mode) {
-    char *dot = strrchr(path, '.');
-    if (dot && strcmp(dot, ".mkv") == 0) {
-        return orig_open(path, flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-    }
-    else {
-        return orig_open(path, flags, mode);
-    }
+int open(const char *path, int flags, mode_t mode)
+{
+    return orig_open(path, flags, mode | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+}
+
+/*
+ * Wrapper for the mkdir function.
+ */
+int mkdir(const char *pathname, mode_t mode)
+{
+    return orig_mkdir(pathname, mode | S_IRWXU | S_IRWXG | S_IRWXO);
 }
