4a578fe663efca7d56de7e4cb8a1fa8bfe771458
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-11-08 07:08:34 -0500

    Make sure the build can be interrupted with CTRL-C.

--- makemkv-builder/run.sh
+++ makemkv-builder/run.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR="$(readlink -f "$(dirname "$0")")"
 BUILDER_DOCKER_IMAGE="ubuntu:xenial"
 
-exec docker run --rm \
+exec docker run --rm -ti \
     -v "$SCRIPT_DIR:/output" \
     -v "$SCRIPT_DIR/builder:/builder:ro" \
     "$BUILDER_DOCKER_IMAGE" /builder/build.sh /output
