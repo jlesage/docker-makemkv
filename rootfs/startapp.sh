6c80693494506fbf04217a677fce2086dd562414
Jocelyn Le Sage <jocelyn@le-sage.com>
2021-12-09 18:55:57 -0500

    Added the `QT_DEBUG_PLUGINS` environment variable as reference.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -3,5 +3,8 @@
 #       opening files.
 # NOTE: $HOME variable needs to be set because the only configuration file
 #       location MakeMKV looks for seems to be "$HOME/.MakeMKV"
+
+#export QT_DEBUG_PLUGINS=1
+
 cd /storage
 exec env HOME=/config LD_PRELOAD=/opt/makemkv/lib/umask_wrapper.so /opt/makemkv/bin/makemkv
