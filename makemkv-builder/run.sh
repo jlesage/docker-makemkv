bddbbd38967565f911d71a81d0a6a75785454773
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-02-11 23:15:15 -0500

    Removed files that are no longer used.

--- makemkv-builder/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-SCRIPT_DIR="$(readlink -f "$(dirname "$0")")"
-BUILDER_DOCKER_IMAGE="ubuntu:bionic"
-
-exec docker run --rm \
-    -v "$SCRIPT_DIR:/output" \
-    -v "$SCRIPT_DIR/builder:/builder:ro" \
-    "$BUILDER_DOCKER_IMAGE" /builder/build.sh /output
