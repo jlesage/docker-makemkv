fde1c9d91bf18d137f7c70d86e05773951146080
Josiah Kerley <josiahkerley@gmail.com>
2024-09-18 23:27:20 -0600

    Update 54-check-optical-drive.sh

--- rootfs/etc/cont-init.d/54-check-optical-drive.sh
+++ rootfs/etc/cont-init.d/54-check-optical-drive.sh
@@ -2,6 +2,7 @@
 set -x
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
+set -o pipefail
 
 echo "looking for usable optical drives..."
 
