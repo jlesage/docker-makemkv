90021526e488f07bab70b2b0209ae50f5074cfec
Jocelyn Le Sage <jocelyn@le-sage.com>
2025-06-21 11:34:40 -0400

    Prevent error message in log about missing swrast_dri.so.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -7,6 +7,15 @@
 #export QT_DEBUG_PLUGINS=1
 export HOME=/config
 
+# Added to avoid the following error message:
+#   MESA-LOADER: failed to open swrast: Error loading shared library
+#   /usr/lib/xorg/modules/dri/swrast_dri.so: No such file or directory
+#   (search paths /usr/lib/xorg/modules/dri, suffix _dri)
+# We could instead install `mesa-dri-gallium`, but this increases the image
+# size a lot.
+export QT_QPA_PLATFORM=xcb
+export QT_XCB_GL_INTEGRATION=none
+
 DEBUG_ARGS=
 if is-bool-val-true "${CONTAINER_DEBUG:-0}"; then
     mkdir -p /config/log/makemkv
